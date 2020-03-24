8e28dd43cbbbf710de1a03c8134e644c0b954749
JamesCodes <buschj@uoguelph.ca>
2020-03-23 22:30:50 -0400

    Added shell of main for testing tomorrow

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*********************
+ * James Busch
+ * 24/03/20
+ * 
+ * This is the tester for the Data_Structures API.
+ * As I build each data structure I will push the current tester
+ * to show iterative testing.
+ * ******************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Linked_List.h"
+
+int main(){
+
+    return 0;
+}
